49464af3a474e0822def6dbd89dac0345678ec01
duchc003 <duchc03.developer@gmail.com>
2022-11-20 00:08:21 +0700

    thay đổi

---
+++ src/util/Application.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.fplhn.sof203;
+
+import edu.fplhn.sof203.view.XeMayFrame;
+
+/**
+ *
+ * @author Phong
+ */
+public class Application {
+    
+    public static void main(String[] args) {
+        new XeMayFrame().setVisible(true);
+    }
+}
