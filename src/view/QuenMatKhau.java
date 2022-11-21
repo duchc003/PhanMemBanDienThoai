77f34ad9124592ba40003ffbe109873ea2bd7d35
HieuNM151 <hieunm151@gmail.com>
2022-11-21 12:34:37 +0700

    update

--- src/view/QuenMatKhau.java
+++ src/view/QuenMatKhau.java
@@ -220,12 +220,11 @@ public class QuenMatKhau extends javax.swing.JFrame {
             }
             String email = lists.get(0).toString();
 
-            final String username = "duchcph22577@fpt.edu.vn";
-            final String password = "congduc003";
+            final String username = "hieunmph22569@fpt.edu.vn";
+            final String password = "15012003";
             Properties prop = new Properties();
             prop.setProperty("mail.smtp.host", "smtp.gmail.com");
             prop.setProperty("mail.smtp.port", "587");
-            prop.setProperty("mail.smtp.port", "465");
             prop.setProperty("mail.smtp.starttls.enable", "true");
             prop.setProperty("mail.smtp.auth", "true");
 
@@ -237,7 +236,7 @@ public class QuenMatKhau extends javax.swing.JFrame {
             });
             try {
                 Message message = new MimeMessage(session);
-                message.setFrom(new InternetAddress("duchcph22577@fpt.edu.vn"));
+                message.setFrom(new InternetAddress("hieunmph22569@fpt.edu.vn"));
                 message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
                 message.setSubject("Mã xác nhận");
                 message.setText("Mã xác nhận của bạn là : " + maXacNhan
